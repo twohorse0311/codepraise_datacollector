e2713f69c40f6751a390f640393119571c55a3e0
Fong <fong54388@gmail.com>
2024-02-16 10:18:28 +0800

    remove comment

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -30,28 +30,6 @@ module CodePraise
             routing.redirect "project/#{project.owner.username}/#{project.name}"
           end
         end
-        #   routing.on 'api/v1/commits' do
-        #     routing.get String, String do |username, projname|
-        #       github_token = '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'
-        #       final_commits = []
-
-        #       (2014..2023).each do |year|
-        #         # 對每個年份構造 since 和 until 參數
-        #         since = "#{year}-01-01T00:00:00Z"
-        #         until_param = "#{year}-12-31T23:59:59Z"
-        #         url = "https://api.github.com/repos/#{username}/#{projname}/commits?since=#{since}&until=#{until_param}&per_page=1"
-
-        #         begin
-        #           response = RestClient.get(url, { 'Authorization' => "token #{github_token}" })
-        #           commits = JSON.parse(response.body)
-        #           final_commits.concat(commits) unless commits.empty?
-        #         rescue RestClient::ExceptionWithResponse => e
-        #           puts "Failed to fetch commits for #{year}: #{e.response}"
-        #         end
-        #       end
-
-        #       final_commits # 返回每年最後一筆 commit 的集合
-        #     end
       end
     end
   end
